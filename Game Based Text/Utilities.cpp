5f4439cb64f513b0806d7ecba4526f4966445298
stanley francois <stanleyon7@gmail.com>
2019-03-23 19:47:16 -0400

    Update Utilities.cpp

--- Game Based Text/Utilities.cpp
+++ Game Based Text/Utilities.cpp
@@ -3,7 +3,45 @@
 void displayStory() {
 	//Story goes here
 
+	std::cout << "################### STALAG ESCAPE ########################" << std::endl;
+	std::cout << "    During korean war, you are a usmarine, who fell over    " << std::endl;
+	std::cout << " enemy airspace, and held as a prisoner of war behind    " << std::endl;
+	std::cout << " enemy lines at korean Stalag (POW camp). As you move   " << std::endl;
+	std::cout << " through the camp, you collect items to help you escape,  " << std::endl;
+	std::cout << " learn a few korean words, and find different potential " << std::endl;
+	std::cout << " escape routes, but there is only one way to succeed.  If " << std::endl;
+	std::cout << " you take the wrong escape route or are miss-prepared, you " << std::endl;
+	std::cout << "will be captured and the game is over. You have an assault pack" << std::endl;
+	std::cout << " that can hold 4 items. You start the game with a pack of " << std::endl;
+	std::cout << " cigarettes inside your pack.  You have 24 hours to escape" << std::endl;
+	std::cout << " before the korean arrives to cut you loose then  send" << std::endl;
+	std::cout << "  you on a forced wintertime march to another camp.    " << std::endl;
+	std::cout << "##########################################################" << std::endl;
+	std::cout << std::endl;
+
 }
 void displayMap() {
-	//Story goes here
+
+	std::cout << "                                                        " << std::endl;
+	std::cout << "                      Korean CAMP' MAP                       " << std::endl;
+	std::cout << "                                                        " << std::endl;
+	std::cout << " ###################################################### " << std::endl;
+	std::cout << " #                                   C Wire AND ELECTRIC PERIMETER  # " << std::endl;
+	std::cout << " #     --------------          -----------            # " << std::endl;
+	std::cout << " #     |             |         |          |           # " << std::endl;
+	std::cout << " #     | Barracks    |         | Messhall |           # " << std::endl;
+	std::cout << " #     |             |         |          |           # " << std::endl;
+	std::cout << " #      -------------          -----------            # " << std::endl;
+	std::cout << " #                                                    # " << std::endl;
+	std::cout << " #     -----------             -----------            # " << std::endl;
+	std::cout << " #     |          |            |          |           # " << std::endl;
+	std::cout << " #     | school house  |               | PFT and CFT|           # " << std::endl;
+	std::cout << " #     |          |                  |       |      Running  Field     |           # " << std::endl;
+	std::cout << " #      -----------            |          |           # " << std::endl;
+	std::cout << " #                              ----------            # " << std::endl;
+	std::cout << " ###################################################### " << std::endl;
+	std::cout << "      *      *           *       *      *    *   *      " << std::endl;
+	std::cout << "        *         *   Nasty Desert   *    *      *       *    " << std::endl;
+	std::cout << "                                                        " << std::endl;
+}
 };
